6c67ab81750d026b929f642c6839207cd6f19196
Favixx <bogdandom52@gmail.com>
2023-06-26 18:30:30 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -49,7 +49,7 @@ export default class ContactForm extends Component {
                             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                             required
-                            value={this.state.name}
+                            value={name}
                         />
                     </label>
                     <label>
@@ -61,7 +61,7 @@ export default class ContactForm extends Component {
                             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                             required
-                            value={this.state.number}
+                            value={number}
                         />
                     </label>
                     <button type="submit">Add contact</button>
